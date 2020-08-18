9f70b172bcb57813b6933691ad18dc442c58a13a
KirtiVelankar <kirti@streamsets.com>
2020-08-18 11:44:40 -0700

    Initial commit

---
+++ conftest.py
@@ -0,0 +1,38 @@
+import logging
+
+import pytest
+
+logger = logging.getLogger('streamsets.ci_cd_poc')
+
+
+def pytest_addoption(parser):
+    parser.addoption('--pipeline-id')
+    parser.addoption('--upgrade-jobs', action='store_true')
+
+
+@pytest.fixture(scope='session')
+def sch(sch_session):
+    yield sch_session
+
+
+@pytest.fixture(scope='session')
+def pipeline(sch, request):
+    pipeline_id = request.config.getoption('pipeline_id')
+    pipeline_ = sch.pipelines.get(pipeline_id=pipeline_id)
+
+    yield pipeline_
+
+    jobs_to_delete = sch.jobs.get_all(pipeline_id=pipeline_.pipeline_id, description='Kirti CI/CD test job')
+    if jobs_to_delete:
+        logger.debug('Deleting test jobs: %s ...', ', '.join(str(job) for job in jobs_to_delete))
+        sch.delete_job(*jobs_to_delete)
+    if not request.session.testsfailed:
+        if request.config.getoption('upgrade_jobs'):
+            pipeline_ = sch.pipelines.get(pipeline_id=pipeline_id)
+            jobs_to_upgrade = [job for job in sch.jobs.get_all(pipeline_id=pipeline_id)
+                               if job.pipeline_commit_label != f'v{pipeline_.version}']
+            if jobs_to_upgrade:
+                logger.info('Upgrading jobs: %s ...', ', '.join(str(job) for job in jobs_to_upgrade))
+                sch.upgrade_job(*jobs_to_upgrade)
+            else:
+                logger.warning('No jobs need to be upgraded')
